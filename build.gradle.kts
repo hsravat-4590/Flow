5cef77a536625fd716ccfe872a26b00ccc2c20d3
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2022-11-29 10:39:57 +0000

    WIP: Publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("multiplatform") version "1.7.10"
+    id("maven-publish")
 }
 
-group = "com.ravathanzalah"
+group = "com.ravathanzalah.flow"
 version = "1.0.0"
+
 
 repositories {
     mavenCentral()
@@ -49,5 +51,82 @@ kotlin {
         val jsTest by getting
 //        val nativeMain by getting
 //        val nativeTest by getting
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.ravathanzalah"
+            artifactId = "flow"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    publications.withType<MavenPublication> {
+    }
+
+    // Configure maven central repository
+    repositories {
+        maven {
+            name = "snapshots"
+            setUrl("https://oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
+
+        maven {
+            name = "sonatype"
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+            mavenContent {
+                releasesOnly()
+            }
+        }
+    }
+
+    // Configure all publications
+    publications.withType<MavenPublication> {
+        // Stub javadoc.jar artifact
+        artifact(javadocJar.get())
+
+        // Provide artifacts information requited by Maven Central
+        pom {
+            name.set("Kotlin Flow Library")
+            description.set("Simple, Intuitive flow management library for Kotlin with a DSL")
+            url.set("https://github.com/hsravat-4590/Flow")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                }
+            }
+            scm {
+                url.set("https://github.com/hsravat-4590/Flow")
+            }
+
+        }
     }
 }
